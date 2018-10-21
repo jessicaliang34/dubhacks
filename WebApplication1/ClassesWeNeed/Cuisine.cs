c279e30a022e699690b1f4f6b069bfa154b11523
Jessica I-Wen Liang <jliang.304@gmail.com>
2018-10-21 00:48:03 -0700

    code commits

---
+++ WebApplication1/ClassesWeNeed/Cuisine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.ClassesWeNeed
+{
+    public class Cuisine
+    {
+		public bool comfortfood { get; set; }
+		public bool newcanadian { get; set; }
+		public bool bars{ get; set; }
+		public bool asianfusion { get; set; }
+		public bool seafood { get; set; }
+		public bool tapas { get; set; }
+		public bool japanese { get; set; }
+		public bool pubs { get; set; }
+		public bool cambodian { get; set; }
+		public bool vietnamese { get; set; }
+		public bool sushi { get; set; }
+		public bool breakfast_brunch { get; set; }
+		public bool korean { get; set; }
+		public bool italian { get; set; }
+		public bool belgian { get; set; }
+		public bool chinese { get; set; }
+	}
+}

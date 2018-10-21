d4d9d4e701fccf04711693c4cb2267b8ff95753d
Jessica I-Wen Liang <jliang.304@gmail.com>
2018-10-20 21:04:59 -0700

    configs
    configs

---
+++ WebApplication1/Models/RestuarantInformationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class RestuarantInformationModel
+	{
+		List<bool> Cuisines { get; set; }
+
+		int MaxPrice { get; set; }
+
+		string CityLocation { get; set; }
+    }
+}
